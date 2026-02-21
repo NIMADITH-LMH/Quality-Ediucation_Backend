9332bb00d0d19151d8871296de04742ecb1b3402
YourName <havindunimadith7@gmail.com>
2026-02-21 21:24:50 +0530

    Delete ValidatorMiddleware and customErrors
    Remove two deleted files: Middleware/ValidatorMiddleware.js (skeleton validateRegisterInput and validateLoginInput middleware) and errors/customErrors.js (custom Error subclasses mapping to http-status-codes). These were placeholder implementations and have been removed, likely as part of a refactor or replacement of validation/error handling.

--- Middleware/ValidatorMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const validateRegisterInput = (req, res, next) => {
-    // Skeleton validator - to be implemented with express-validator or similar
-    next();
-};
-
-export const validateLoginInput = (req, res, next) => {
-    // Skeleton validator - to be implemented with express-validator or similar
-    next();
-};
